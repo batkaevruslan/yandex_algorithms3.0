a16e9022f0543d16e09afa979805c35d9d5fe54c
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-01 08:53:34 +0300

    Add 4.Test problem solution

---
+++ YandexAlgorithmsContest/4.Test/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace _4.Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            double pupilCount = double.Parse(sr.ReadLine()!);
+            double testCount = double.Parse(sr.ReadLine()!);
+            double petyaRow = double.Parse(sr.ReadLine()!);
+            int petyaColumn = int.Parse(sr.ReadLine()!);
+
+            string result = ProblemSolver.FindPlaceForVasya(pupilCount, testCount, petyaRow, petyaColumn);
+            
+            File.WriteAllText("output.txt", result);
+        }
+        public static class ProblemSolver
+        {
+
+            public static string FindPlaceForVasya(double pupilCount, double testCount, double petyaRow, int petyaColumn)
+            {
+                double petyaPlace = (petyaRow - 1) * 2 + petyaColumn;
+
+                double bestPlaceBehind = petyaPlace + testCount;
+                double bestPlaceInFront = petyaPlace - testCount;
+                double possibleVasyaPlace = bestPlaceBehind;
+                if (bestPlaceBehind > pupilCount)
+                {
+                    if (bestPlaceInFront <= 0)
+                    {
+                        return "-1";
+                    }
+                    possibleVasyaPlace = bestPlaceInFront;
+                }
+                else if (bestPlaceInFront > 0 
+                    && (petyaRow - GetRowForPlace(bestPlaceInFront)) < (GetRowForPlace(bestPlaceBehind) - petyaRow))
+                {
+                    possibleVasyaPlace = bestPlaceInFront;
+                }
+
+                double vasyaRow = GetRowForPlace(possibleVasyaPlace);
+                double vasyaColumn = possibleVasyaPlace % 2 == 0 ? 2 : 1;
+                return $"{vasyaRow} {vasyaColumn}";
+
+            }
+
+            private static double GetRowForPlace(double placeNumber)
+            {
+                return placeNumber % 2 == 0 ? Math.Floor(placeNumber / 2) : Math.Floor(placeNumber / 2) + 1;
+            }
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/4.Test/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Test
+{
+    internal class Tests
+    {
+        [TestCase(25, 2, 1, 2, "2 2")]
+        [TestCase(25, 13, 7, 1, "-1")]
+        [TestCase(2, 2, 1, 1, "-1")]
+        [TestCase(2, 2, 1, 2, "-1")]
+        [TestCase(5, 4, 2, 1, "-1")]
+        [TestCase(5, 4, 3, 1, "1 1")]
+        [TestCase(15, 5, 3, 1, "5 2")]
+        [TestCase(15, 5, 5, 2, "3 1")]
+        [TestCase(15, 5, 5, 1, "7 2")]
+        [TestCase(15, 5, 8, 1, "5 2")]
+        [TestCase(15, 15, 8, 1, "-1")]
+        public void FindsVasyaPlace(double pupilCount, double testCount, double petyaRow, int petyaColumn, string expectedResult)
+        {
+            string result = Program.ProblemSolver.FindPlaceForVasya(pupilCount, testCount, petyaRow, petyaColumn);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
