ffc7e9be44952841802a39341146a552e1631c59
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-06 07:27:04 +0300

    Add 5.GoodString problem solution

---
+++ YandexAlgorithmsContest/5.GoodString/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _5.GoodString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            byte letterCount = byte.Parse(sr.ReadLine()!);
+            double[] countPerLetter = new double[letterCount];
+            for (int i = 0; i < letterCount; i++)
+            {
+                countPerLetter[i] = double.Parse(sr.ReadLine()!);
+            }
+            var result = ProblemSolver.GetGoodness(0, countPerLetter).ToString();
+            File.WriteAllText("output.txt", result);
+        }
+
+        public class ProblemSolver
+        {
+            internal static double GetGoodness(double usedLetters, double[] countPerLetter)
+            {
+                double minLetterCountInCurrentRange = countPerLetter.Min();
+                double goodness = (minLetterCountInCurrentRange - usedLetters) * (countPerLetter.Length - 1);
+                List<double> countPerLetterList = countPerLetter.ToList();
+                int lastStartIndex = 0;
+                int lastEndIndex = 0;
+                int nextMinLetterIndex = countPerLetterList.IndexOf(minLetterCountInCurrentRange);
+                while (nextMinLetterIndex > -1)
+                {
+                    if (nextMinLetterIndex > lastStartIndex)
+                    {
+                        goodness += GetGoodness(minLetterCountInCurrentRange, countPerLetter[lastStartIndex..nextMinLetterIndex]);
+                    }
+                    lastStartIndex = nextMinLetterIndex + 1;
+                    lastEndIndex = nextMinLetterIndex;
+                    nextMinLetterIndex = countPerLetterList.IndexOf(minLetterCountInCurrentRange, lastStartIndex);
+                }
+                if (lastEndIndex + 1 < countPerLetter.Length - 1)
+                {
+                    goodness += GetGoodness(minLetterCountInCurrentRange, countPerLetter[(lastEndIndex + 1)..]);
+                }
+                return goodness;
+            }
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/5.GoodString/Tests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.GoodString
+{
+    internal class Tests
+    {
+        private static TestCaseData[] _cases = new TestCaseData[]
+        {
+            new TestCaseData(new double[]{1}, 0),
+            new TestCaseData(new double[]{1, 1}, 1),
+            new TestCaseData(new double[]{1, 1, 1}, 2),
+            new TestCaseData(new double[]{3, 4}, 3),
+            new TestCaseData(new double[]{1, 0, 1}, 0),
+            new TestCaseData(new double[]{2, 0, 2}, 0),
+            new TestCaseData(new double[]{1, 1, 0, 1, 1}, 2),
+            new TestCaseData(new double[]{1, 1, 0, 0, 1, 1}, 2),
+            new TestCaseData(new double[]{0, 1, 1, 0}, 1),
+            new TestCaseData(new double[]{2, 3, 1, 2, 3, 1, 2, 3}, 10),
+        };
+
+        [TestCaseSource(nameof(_cases))]
+        public void ReturnsGoodness(double[] countPerLetter, double expectedGoodness)
+        {
+            var result = Program.ProblemSolver.GetGoodness(0, countPerLetter);
+
+            Assert.That(result, Is.EqualTo(expectedGoodness));
+        }
+    }
+}
