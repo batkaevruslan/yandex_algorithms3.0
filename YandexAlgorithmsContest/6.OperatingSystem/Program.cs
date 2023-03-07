1b96d02e861091a7fe72bbb69064d7cfa3953110
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-07 07:23:45 +0300

    Add 6.OperatingSustem problem solution

---
+++ YandexAlgorithmsContest/6.OperatingSystem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _6.OperatingSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            double sectorCount = double.Parse(sr.ReadLine()!);
+            short osCount = short.Parse(sr.ReadLine()!);
+            List<OS> operatingSystems = new(osCount);
+            for (int i = 0; i < osCount; i++)
+            {
+                double[] range = sr.ReadLine()!.Split(' ').Select(double.Parse).ToArray();
+                operatingSystems.Add(new OS(StartSector: range[0], EndSector: range[1]));
+            }
+
+            int result = GetWorkingOS(operatingSystems, sectorCount).Count;
+            File.WriteAllText("output.txt", result.ToString());
+        }
+        private static bool OSOverlap(OS first, OS second)
+        {
+            return first.StartSector <= second.StartSector && second.StartSector <= first.EndSector
+                || first.StartSector <= second.EndSector && second.EndSector <= first.EndSector
+                || second.StartSector <= first.StartSector && first.StartSector <= second.EndSector
+                || second.StartSector <= first.EndSector && first.EndSector <= second.EndSector;
+        }
+        public static List<OS> GetWorkingOS(List<OS> oses, double sectorCount)
+        {
+            if (oses.Count == 0) { return new List<OS>(); }
+            if (oses.Count == 1) { return oses; }
+            List<OS> workingOS = new(oses.Count) { oses[^1] };
+            for (int i = 0; i < oses.Count - 1; i++)
+            {
+                OS firstOS = oses[i];
+                bool isFirstOSWorking = true;
+                for (int j = i + 1; j < oses.Count; j++)
+                {
+                    OS secondOS = oses[j];
+                    if (firstOS.StartSector <= firstOS.EndSector)
+                    {
+                        if (secondOS.StartSector <= secondOS.EndSector)
+                        {
+                            isFirstOSWorking = !OSOverlap(firstOS, secondOS);
+                        }
+                    }
+                    else
+                    {
+                        if (secondOS.StartSector <= secondOS.EndSector)
+                        {
+                            isFirstOSWorking = !OSOverlap(new OS(firstOS.StartSector, sectorCount), secondOS)
+                                && !OSOverlap(new OS(0, firstOS.EndSector), secondOS);
+                        } else
+                        {
+                            isFirstOSWorking = !OSOverlap(new OS(firstOS.StartSector, sectorCount), new OS(secondOS.StartSector, sectorCount
+                                )) 
+                                && !OSOverlap(new OS(0, firstOS.EndSector), new OS(0, secondOS.EndSector));
+                        }
+                    }
+                    if (!isFirstOSWorking)
+                    {
+                        break;
+                    }
+                } 
+                if (isFirstOSWorking)
+                {
+                    workingOS.Add(firstOS);
+                }
+            }
+            return workingOS;
+        }
+
+        public record struct OS(double StartSector, double EndSector);
+    }
+}
---
+++ YandexAlgorithmsContest/6.OperatingSystem/Tests.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _6.OperatingSystem.Program;
+
+namespace _6.OperatingSystem
+{
+    internal class Tests
+    {
+        private static TestCaseData[] _cases = new TestCaseData[]
+        {
+            new TestCaseData(
+                new OS[]{new (1, 1)},
+                new OS[]{new OS(1,1)}),
+            new TestCaseData(
+                new OS[]{new (0, 0), new (1, 1)},
+                new OS[]{new OS(0,0), new (1, 1)}),
+            new TestCaseData(
+                new OS[]{new (0, 0), new (0, 0)},
+                new OS[]{new OS(0,0)}),
+            new TestCaseData(
+                new OS[]{new (0, 2), new (1, 3)},
+                new OS[]{new OS(1, 3)}),
+            new TestCaseData(
+                new OS[]{new (1, 1), new (0, 2)},
+                new OS[]{new OS(0, 2)}),
+            new TestCaseData(
+                new OS[]{new (0, 2), new (1, 1)},
+                new OS[]{new OS(1, 1)}),
+            new TestCaseData(
+                new OS[]{new (10, 5), new (6, 7)},
+                new OS[]{new (10, 5), new (6, 7)}),
+            new TestCaseData(
+                new OS[]{new (6, 7), new (10, 5)},
+                new OS[]{new (6, 7), new (10, 5)}),
+            new TestCaseData(
+                new OS[]{new (10, 5), new (9, 6)},
+                new OS[]{new (9, 9)}),
+            new TestCaseData(
+                new OS[]{new OS(1, 3), new (4, 7), new (3, 4) },
+                new OS[]{new (3, 4)})
+        };
+
+        [TestCaseSource(nameof(_cases))]
+        public void ReturnsWorkingOS(OS[] installedOS, OS[] expectedWorkingOS)
+        {
+            List<OS> result = Program.GetWorkingOS(installedOS.ToList(), 20);
+
+            result.Should().BeEquivalentTo(expectedWorkingOS);
+        }
+    }
+}
