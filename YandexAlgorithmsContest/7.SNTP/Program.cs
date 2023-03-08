a7df83e44eb1d01c53a7f93a5e3739744e634b3e
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-08 07:39:35 +0300

    Add 7.SNTP problem solution

---
+++ YandexAlgorithmsContest/7.SNTP/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace _7.SNTP
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            Time sendTime = Time.Parse(sr.ReadLine());
+            Time serverTime = Time.Parse(sr.ReadLine());
+            Time receiveTime = Time.Parse(sr.ReadLine());
+
+            File.WriteAllText("output.txt", GetAdjustedTime(sendTime, serverTime, receiveTime).ToString());
+        }
+
+        public static Time GetAdjustedTime(Time sendTime, Time serverTime, Time receiveTime)
+        {
+            int requestLatency = receiveTime.GetTotalSeconds() - sendTime.GetTotalSeconds();
+            if (requestLatency < 0)
+            {
+                requestLatency = requestLatency + new Time(24, 0, 0).GetTotalSeconds();
+            }
+            int adjustedTimeTotalSeconds = serverTime.GetTotalSeconds() + (int)Math.Round(requestLatency / 2.0, MidpointRounding.ToPositiveInfinity);
+            int seconds = adjustedTimeTotalSeconds % 60;
+            int minutes = ((adjustedTimeTotalSeconds - seconds) / 60) % 60;
+            int hours = (adjustedTimeTotalSeconds / 60 / 60) % 24;
+            return new Time(hours, minutes, seconds);
+        }
+
+        public class Time
+        {
+            public Time(int h, int m, int s)
+            {
+                Hour = h;
+                Minute = m;
+                Second = s;
+            }
+
+            public int Hour { get; }
+            public int Minute { get; }
+            public int Second { get; }
+
+            public static Time Parse(string input)
+            {
+                var stringParts = input.Split(":");
+                int h = int.Parse(stringParts[0]);
+                int m = int.Parse(stringParts[1]);
+                int s = int.Parse(stringParts[2]);
+                return new Time(h, m, s);
+            }
+
+            public override string ToString()
+            {
+                return $"{Hour:d2}:{Minute:d2}:{Second:d2}";
+            }
+
+            public int GetTotalSeconds()
+            {
+                return Hour * 60 * 60 + Minute * 60 + Second;
+            }
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/7.SNTP/Tests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _7.SNTP.Program;
+
+namespace _7.SNTP
+{
+    internal class Tests
+    {
+        private static TestCaseData[] _cases = new TestCaseData[] {
+            new (new Time(15, 1, 00), new Time(18, 09, 45), new Time(15, 1, 40), new Time(18, 10, 05)),
+            new (new Time(23, 59, 50), new Time(12, 00, 00), new Time(0, 0, 10), new Time(12, 00, 10)),
+            new (new Time(23, 59, 50), new Time(23, 59, 59), new Time(0, 0, 10), new Time(00, 00, 09)),
+            new (new Time(23, 59, 59), new Time(23, 59, 59), new Time(23, 59, 59), new Time(23, 59, 59)),
+            new (new Time(0, 0, 0), new Time(0, 0, 10), new Time(0, 0, 1), new Time(00, 00, 11)),
+        };
+
+        [TestCaseSourceAttribute(nameof(_cases))]
+        public void AdjustsTime(Time sendTime, Time serverTime, Time receiveTime, Time expectedAdjustedTime)
+        {
+            GetAdjustedTime(sendTime, serverTime, receiveTime).Should().BeEquivalentTo(expectedAdjustedTime);
+        }
+    }
+}
