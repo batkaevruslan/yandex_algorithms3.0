7d356875cfba778326747509765d31272c7eb005
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-09 08:57:45 +0300

    Add 2.PrettyString problem solution

---
+++ YandexAlgorithmsContest/2.PrettyString/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace _2.PrettyString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            double replacementCount = double.Parse(sr.ReadLine());
+            string word = sr.ReadLine();
+
+            int maxPrettieness = GetMaxPrettiness(replacementCount, word);
+            File.WriteAllText("output.txt", maxPrettieness.ToString());
+        }
+
+        private const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        public static int GetMaxPrettiness(double replacementCount, string word)
+        {
+            int maxPrettieness = 0;
+            foreach (char letter in alphabet)
+            {
+                double replacementLeft = replacementCount;
+                int currentLetterMaxPrettieness = 0;
+                int currentMaxPrettiness = 0;
+                int i = 0, j = 0;
+                while (i < word.Length && j < word.Length)
+                {
+                    if (replacementLeft > 0)
+                    {
+                        if (word[j] == letter)
+                        {
+                            currentMaxPrettiness++;
+                            j++;
+                        }
+                        else
+                        {
+                            currentMaxPrettiness++;
+                            j++;
+                            replacementLeft--;
+                        }
+                    }
+                    else
+                    {
+                        if (word[j] == letter)
+                        {
+                            currentMaxPrettiness++;
+                            j++;
+                        }
+                        else if (word[i] == letter)
+                        {
+                            currentMaxPrettiness--;
+                            i++;
+                        }
+                        else
+                        {
+                            currentMaxPrettiness--;
+                            replacementLeft++;
+                            i++;
+                        }
+                    }
+                    currentLetterMaxPrettieness = Math.Max(currentLetterMaxPrettieness, currentMaxPrettiness);
+                }
+                maxPrettieness = Math.Max(currentLetterMaxPrettieness, maxPrettieness);
+            }
+            return maxPrettieness;
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/2.PrettyString/Tests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.PrettyString
+{
+    internal class Tests
+    {
+        [TestCase(0, "a", 1)]
+        [TestCase(0, "ab", 1)]
+        [TestCase(1, "ab", 2)]
+        [TestCase(1, "aab", 3)]
+        [TestCase(1, "aaa", 3)]
+        [TestCase(1, "bba", 3)]
+        [TestCase(0, "aaa", 3)]
+        [TestCase(2, "cababa", 5)]
+        [TestCase(2, "abcaz", 4)]
+        [TestCase(2, "helto", 3)]
+        public void CalculateMaxPrettieness(double replacementCount, string word, int expectedMaxPrettieness)
+        {
+            Program.GetMaxPrettiness(replacementCount, word).Should().Be(expectedMaxPrettieness);
+        }
+    }
+}
