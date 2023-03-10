75a55df1ded80bf3ca626befaffef5e1dd916d36
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-10 08:49:31 +0300

    Add 14.TrainSort problem solution

---
+++ YandexAlgorithmsContest/14.TrainSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _14.TrainSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            sr.ReadLine();
+            int[] trains = (sr.ReadLine() ?? "").Trim().Split(" ").Select(int.Parse).ToArray();
+            Stack<int> deadEnd = new();
+            int lastSortedTraint = 0;
+            foreach(int train in trains)
+            {
+                if (train == lastSortedTraint + 1)
+                {
+                    lastSortedTraint = train;
+                    while (deadEnd.Count > 0 && deadEnd.Peek() == lastSortedTraint + 1)
+                    {
+                        lastSortedTraint = deadEnd.Pop();
+                    }
+                }
+                else
+                {
+                    deadEnd.Push(train);
+                }
+            }
+
+            File.WriteAllText("output.txt", deadEnd.Count == 0 ? "YES" : "NO");
+        }
+    }
+}
