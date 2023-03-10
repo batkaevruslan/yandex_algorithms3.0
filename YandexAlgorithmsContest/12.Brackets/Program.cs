bf37ac3ce0aac1358e9d083a106bf09100f37a2f
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-10 08:17:45 +0300

    Add 12.Brackets problem solution

---
+++ YandexAlgorithmsContest/12.Brackets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _12.Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            string sequence = sr.ReadLine() ?? "";
+            Stack<char> stack = new();
+            foreach (char c in sequence)
+            {
+                if(stack.Count > 0 && (c == ')' || c == ']' || c == '}'))
+                {
+                    var lastChar = stack.Peek();
+                    char oppositeBracket = c switch
+                    {
+                        ')' => '(',
+                        ']' => '[',
+                        '}' => '{',
+                        _ => throw new NotImplementedException()
+                    };
+                    if(lastChar == oppositeBracket )
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(c);
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            File.WriteAllText("output.txt", stack.Count > 0 ? "no" : "yes");
+        }
+    }
+}
