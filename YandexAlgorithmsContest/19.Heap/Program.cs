51595de268c34de1f1314d0657825c42878c5346
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-13 10:08:06 +0300

    Add 19.Heap problem solution

---
+++ YandexAlgorithmsContest/19.Heap/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace _19.Heap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+            StringBuilder sb = new();
+            MyHeap heap = new();
+            int n = int.Parse(sr.ReadLine()!);
+            for (int i = 0; i < n; i++)
+            {
+                string command = sr.ReadLine()!.Trim();
+                if(command == "1")
+                {
+                    int value = heap.Extract();
+                    sb.AppendLine(value.ToString());
+                }
+                else
+                {
+                    int value = command.Split(" ").Select(int.Parse).Last();
+                    heap.Insert(value);
+                }
+            }
+
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+
+        public class MyHeap
+        {
+            private int[] _data = new int[100000];
+            private int _lastUsedIndex = -1;
+            public int Extract()
+            {
+                int max = _data[0];
+                int parentIndex = 0;
+                _data[parentIndex] = _data[_lastUsedIndex];
+                int secondChildIndex = parentIndex * 2 + 2;
+                int firstChildIndex = parentIndex * 2 + 1;
+                while(parentIndex < _lastUsedIndex && firstChildIndex < _lastUsedIndex)
+                {
+                    if(secondChildIndex < _lastUsedIndex 
+                        && _data[secondChildIndex] > _data[firstChildIndex])
+                    {
+                        firstChildIndex = secondChildIndex;
+                    }
+                    else if (_data[parentIndex] <= _data[firstChildIndex]) 
+                    {
+                        (_data[firstChildIndex], _data[parentIndex]) = (_data[parentIndex], _data[firstChildIndex]);
+                        parentIndex = firstChildIndex;
+                        secondChildIndex = parentIndex * 2 + 2;
+                        firstChildIndex = parentIndex * 2 + 1;
+                    } else
+                    {
+                        break;
+                    }
+                }
+                _lastUsedIndex--;
+                return max;
+            }
+
+            public void Insert(int value)
+            {
+                _data[++_lastUsedIndex] = value;
+                int indexToSwap = _lastUsedIndex;
+                int parentIndex = (indexToSwap - 1) / 2;
+                while (parentIndex >= 0 && _data[indexToSwap] > _data[parentIndex])
+                {
+                    (_data[indexToSwap], _data[parentIndex]) = (_data[parentIndex], _data[indexToSwap]);
+                    indexToSwap = parentIndex;
+                    parentIndex = (indexToSwap - 1) / 2;
+                }
+            }
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/19.Heap/Tests.cs
@@ -0,0 +1,117 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Heap
+{
+    internal class Tests
+    {
+        [Test]
+        public void ToDo()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(100);
+            heap.Extract().Should().Be(100);
+
+
+            heap.Insert(100);
+            heap.Insert(50);
+            heap.Extract().Should().Be(100);
+
+            heap.Insert(100);
+            heap.Extract().Should().Be(100);
+
+            heap.Insert(40);
+            heap.Insert(30);
+            heap.Insert(20);
+            heap.Insert(10);
+            heap.Insert(1);
+            heap.Extract().Should().Be(50);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(1);
+            heap.Insert(345);
+            heap.Extract().Should().Be(345);
+            heap.Insert(4346);
+            heap.Extract().Should().Be(4346);
+            heap.Insert(2435);
+            heap.Extract().Should().Be(2435);
+            heap.Insert(235);
+            heap.Insert(5);
+            heap.Insert(1);
+            heap.Extract().Should().Be(235);
+            heap.Extract().Should().Be(5);
+            heap.Extract().Should().Be(1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(-1);
+            heap.Insert(-2);
+            heap.Insert(0);
+            heap.Extract().Should().Be(0);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(10);
+            heap.Insert(10);
+            heap.Insert(5);
+            heap.Extract().Should().Be(10);
+            heap.Extract().Should().Be(10);
+            heap.Extract().Should().Be(5);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(10);
+            heap.Insert(5);
+            heap.Extract().Should().Be(10);
+            heap.Extract().Should().Be(5);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            Program.MyHeap heap = new();
+            heap.Insert(1);
+            heap.Insert(10);
+            heap.Insert(2);
+            heap.Insert(3);
+            heap.Insert(4);
+            heap.Insert(20);
+            heap.Insert(5);
+            heap.Insert(6);
+            heap.Insert(15);
+            heap.Insert(7);
+            heap.Insert(8);
+            heap.Insert(9);
+            heap.Extract().Should().Be(20);
+            heap.Extract().Should().Be(15);
+            heap.Extract().Should().Be(10);
+            heap.Extract().Should().Be(9);
+            heap.Extract().Should().Be(8);
+            heap.Extract().Should().Be(7);
+            heap.Extract().Should().Be(6);
+            heap.Extract().Should().Be(5);
+            heap.Extract().Should().Be(4);
+            heap.Extract().Should().Be(3);
+            heap.Extract().Should().Be(2);
+            heap.Extract().Should().Be(1);
+        }
+    }
+}
