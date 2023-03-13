6c74bed195cd2bade0b71caf604c2f4e4f86c221
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-13 07:27:48 +0300

    Add 18.Deque problem solution

---
+++ YandexAlgorithmsContest/18.Deque/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace _18.Deque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using FileStream fs = File.OpenRead("input.txt");
+            using StreamReader sr = new StreamReader(fs);
+
+            string line;
+            StringBuilder sb = new();
+            MyDeque deque = new();
+            while ((line = sr.ReadLine()) != "exit")
+            {
+                try
+                {
+                    if (line.StartsWith("push_front"))
+                    {
+                        int n = int.Parse(line.Substring("push_front ".Length));
+                        deque.PushFront(n);
+                        sb.AppendLine("ok");
+                    }
+                    if (line.StartsWith("push_back"))
+                    {
+                        int n = int.Parse(line.Substring("push_back ".Length));
+                        deque.PushBack(n);
+                        sb.AppendLine("ok");
+                    }
+                    else if (line == "pop_front")
+                    {
+                        sb.AppendLine(deque.PopFront().ToString());
+                    }
+                    else if (line == "pop_back")
+                    {
+                        sb.AppendLine(deque.PopBack().ToString());
+                    }
+                    else if (line == "front")
+                    {
+                        sb.AppendLine(deque.Front().ToString());
+                    }
+                    else if (line == "back")
+                    {
+                        sb.AppendLine(deque.Back().ToString());
+                    }
+                    else if (line == "size")
+                    {
+                        sb.AppendLine(deque.Size().ToString());
+                    }
+                    else if (line == "clear")
+                    {
+                        deque.Clear();
+                        sb.AppendLine("ok");
+                    }
+                }
+                catch
+                {
+                    sb.AppendLine("error");
+                }
+            }
+            sb.AppendLine("bye");
+            File.WriteAllText("output.txt", sb.ToString());
+        }
+        public class MyDeque
+        {
+            private int[] _data;
+            private int? _front;
+            private int? _back;
+            public MyDeque()
+            {
+                _data = new int[2];
+            }
+            internal int Back()
+            {
+                if (Size() == 0)
+                {
+                    throw new InvalidOperationException("Deque is empty");
+                }
+                return _data[_back.Value];
+            }
+
+            internal void Clear()
+            {
+                _back = null;
+                _front = null;
+            }
+
+            internal int Front()
+            {
+                if (Size() == 0)
+                {
+                    throw new InvalidOperationException("Deque is empty");
+                }
+                return _data[_front.Value];
+            }
+
+            internal object PopBack()
+            {
+                int size = Size();
+                if (size == 0)
+                {
+                    throw new InvalidOperationException("Deque is empty");
+                }
+                int result = _data[_back.Value];
+                if (size > 1)
+                {
+                    _back = GetCircularBufferIndex(_back.Value - 1);
+                }
+                else
+                {
+                    Clear();
+                }
+                return result;
+            }
+
+            internal int PopFront()
+            {
+                int size = Size();
+                if (size == 0)
+                {
+                    throw new InvalidOperationException("Deque is empty");
+                }
+                int result = _data[_front.Value];
+                if (size > 1)
+                {
+                    _front = GetCircularBufferIndex(_front.Value + 1);
+                }
+                else
+                {
+                    Clear();
+                }
+                return result;
+            }
+
+            internal void PushBack(int n)
+            {
+                if (Size() == _data.Length)
+                {
+                    Resize();
+                }
+                _back = GetCircularBufferIndex((_back ?? 0) + 1);
+                _data[_back.Value] = n;
+                if (!_front.HasValue)
+                {
+                    _front = _back;
+                }
+            }
+
+            internal void PushFront(int n)
+            {
+                if (Size() == _data.Length)
+                {
+                    Resize();
+                }
+                _front = GetCircularBufferIndex((_front ?? 0) - 1);
+                _data[_front.Value] = n;
+                if (!_back.HasValue)
+                {
+                    _back = _front;
+                }
+            }
+
+            private void Resize()
+            {
+                var newData = new int[_data.Length * 2];
+                int size = Size();
+                for (int i = 0; i < size; i++)
+                {
+                    newData[i] = _data[GetCircularBufferIndex(_front.Value + i)];
+                }
+                _data = newData;
+                _front = 0;
+                _back = size - 1;
+            }
+
+            internal int Size()
+            {
+                return !_front.HasValue ? 0 : (_data.Length + _back.Value - _front.Value) % _data.Length + 1;
+            }
+
+            private int GetCircularBufferIndex(int index)
+            {
+                return (_data.Length + index) % _data.Length;
+            }
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/18.Deque/Tests.cs
@@ -0,0 +1,165 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Deque
+{
+    internal class Tests
+    {
+        [Test]
+        public void PushesToFront()
+        {
+            var deque = new Program.MyDeque();
+
+            deque.PushFront(1);
+            deque.Front().Should().Be(1);
+
+            deque.PushFront(2);
+            deque.Front().Should().Be(2);
+
+            deque.PushFront(3);
+            deque.Front().Should().Be(3);
+
+            deque.PushFront(4);
+            deque.Front().Should().Be(4);
+
+            deque.PushFront(5);
+            deque.Front().Should().Be(5);
+
+            deque.PushFront(6);
+            deque.Front().Should().Be(6);
+
+            deque.PushFront(7);
+            deque.Front().Should().Be(7);
+        }
+
+        [Test]
+        public void ReturnsSize()
+        {
+            var deque = new Program.MyDeque();
+
+            deque.Size().Should().Be(0);
+
+            deque.PushFront(1);
+            deque.Size().Should().Be(1);
+
+            deque.PushFront(2);
+            deque.Size().Should().Be(2);
+
+            deque.PushFront(3);
+            deque.Size().Should().Be(3);
+
+            deque.PushFront(4);
+            deque.Size().Should().Be(4);
+
+            deque.PushFront(5);
+            deque.Size().Should().Be(5);
+
+            deque.PushFront(6);
+            deque.Size().Should().Be(6);
+
+            deque.PushFront(7);
+            deque.Size().Should().Be(7);
+        }
+
+        [Test]
+        public void PopsFront()
+        {
+            var deque = new Program.MyDeque();
+
+            deque.PushFront(1);
+            deque.PopFront();
+            deque.Size().Should().Be(0);
+
+            deque.PushFront(1);
+            deque.PopFront();
+            deque.Size().Should().Be(0);
+
+            deque.PushFront(1);
+            deque.PushFront(2);
+            deque.PopFront();
+            deque.Size().Should().Be(1);
+            deque.Front().Should().Be(1);
+
+            deque.PushFront(2);
+            deque.PushFront(3);
+            deque.PopFront();
+            deque.Size().Should().Be(2);
+            deque.Front().Should().Be(2);
+
+            deque.PushFront(3);
+            deque.PushFront(4);
+            deque.PopFront();
+            deque.Size().Should().Be(3);
+            deque.Front().Should().Be(3);
+        }
+
+        [Test]
+        public void PushesToBack()
+        {
+            var deque = new Program.MyDeque();
+
+            deque.PushBack(1);
+            deque.Back().Should().Be(1);
+
+            deque.PushBack(2);
+            deque.Back().Should().Be(2);
+
+            deque.PushBack(3);
+            deque.Back().Should().Be(3);
+
+            deque.PushBack(4);
+            deque.Back().Should().Be(4);
+
+            deque.PushBack(5);
+            deque.Back().Should().Be(5);
+
+            deque.PushBack(6);
+            deque.Back().Should().Be(6);
+
+            deque.PushBack(7);
+            deque.Back().Should().Be(7);
+        }
+
+        [Test]
+        public void PopsBack()
+        {
+            var deque = new Program.MyDeque();
+
+            deque.PushBack(1);
+            var back = deque.PopBack();
+            back.Should().Be(1);
+            deque.Size().Should().Be(0);
+
+            deque.PushBack(1);
+            back = deque.PopBack();
+            back.Should().Be(1);
+            deque.Size().Should().Be(0);
+
+            deque.PushBack(1);
+            deque.PushBack(2);
+            back = deque.PopBack();
+            back.Should().Be(2);
+            deque.Size().Should().Be(1);
+            deque.Back().Should().Be(1);
+
+            deque.PushBack(2);
+            deque.PushBack(3);
+            back = deque.PopBack();
+            back.Should().Be(3);
+            deque.Size().Should().Be(2);
+            deque.Back().Should().Be(2);
+
+            deque.PushBack(3);
+            deque.PushBack(4);
+            back = deque.PopBack();
+            back.Should().Be(4);
+            deque.Size().Should().Be(3);
+            deque.Back().Should().Be(3);
+        }
+    }
+}
