a8c7c664cdac967aee4d6ad598390ec2d69f0bef
Ruslan Batkaev <batkaevruslan@gmail.com>
2023-03-17 08:26:17 +0300

    Solve 22.Grasshopper

---
+++ YandexAlgorithmsContest/22.Grasshopper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _22.Grasshopper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Trim().Split(" ").Select(int.Parse).ToArray();
+            int numberOfCells = input[0];
+            int longestHop = input[1];
+            int result = GetPossibleHopCount(numberOfCells, longestHop);
+            Console.WriteLine(result);
+        }
+
+        public static int GetPossibleHopCount(int numberOfCells, int longestHop)
+        {
+            int[] dp = new int[Math.Max(2, numberOfCells)];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (int i = 2; i < numberOfCells; i++)
+            {
+                for (int j = Math.Max(1, i - longestHop); j < i; j++)
+                {
+                    dp[i] += dp[j];
+                }
+                if (i <= longestHop)
+                {
+                    dp[i]++;
+                }
+                
+            }
+
+            return dp[numberOfCells - 1];
+
+        }
+    }
+}
---
+++ YandexAlgorithmsContest/22.Grasshopper/Tests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Grasshopper
+{
+    internal class Tests
+    {
+        [TestCase(1, 1, 1)]
+        [TestCase(2, 1, 1)]
+        [TestCase(8, 2, 21)]
+        [TestCase(6, 3, 13)]
+        [TestCase(6, 4, 15)]
+        [TestCase(1, 10, 1)]
+        public void ReturnsPossibleHopCount(int nuberOfCells, int longestHop, int expectedHopCount)
+        {
+            Program.GetPossibleHopCount(nuberOfCells, longestHop).Should().Be(expectedHopCount);
+        }
+    }
+}
